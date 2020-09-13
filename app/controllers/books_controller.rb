63c49c178880f790df95ea2efa997f7d2564aafa
sd1104 <67669319+sd1104@users.noreply.github.com>
2020-09-14 07:08:58 +0900

    Revert "new create function"

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -2,28 +2,4 @@ class BooksController < ApplicationController
   def index
     @books = Book.all
   end
-
-  def new
-    @book = Book.new
-  end
-
-  def create
-    @book = Book.new(book_params)
-    if @book.save
-      redirect_to books_path
-    else
-      render :new
-    end
-  end
-
-  private
-  def book_params
-    params.required(:book).permit(
-      :title,
-      :author,
-      :publisher,
-      :genre
-    )
-  end
-
 end
