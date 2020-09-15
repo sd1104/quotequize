fd62e65503c65ecba0031ca1fb462abdb07ba05a
sd1104 <67669319+sd1104@users.noreply.github.com>
2020-09-16 07:30:52 +0900

    getter setter

--- app/javascript/packs/hello_vue_calc.js
+++ app/javascript/packs/hello_vue_calc.js
@@ -6,10 +6,24 @@ Vue.config.productionTip = false
 const calc = new Vue ({
   el: '#calc',
   data: {
-    message: "hello"
+    message: "hello",
+    basePrice: 100
   },
   computed: {
     reversedMessage: function() {
+      return this.message.split('').reverse().join('')
+    },
+    taxIncludedPrice: {
+      get: function() {
+        return parseInt(this.basePrice * 1.08)
+      },
+      set: function(taxIncludedPrice) {
+        this.basePrice = Math.ceil(taxIncludedPrice / 1.08)
+      }
+    }
+  },
+  methods: {
+    reversedMessageMethod: function() {
       return this.message.split('').reverse().join('')
     }
   }
