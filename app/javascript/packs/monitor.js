a9406540f57cf352e91c76bcdaad89b87f265319
sd1104 <67669319+sd1104@users.noreply.github.com>
2020-09-16 22:46:45 +0900

    done carricuram

--- app/javascript/packs/monitor.js
+++ app/javascript/packs/monitor.js
@@ -9,7 +9,15 @@ const monitor = new Vue ({
     message: 'hello',
     km: 0,
     m: 0,
-    mm: 0
+    mm: 0,
+    firstName: '',
+    lastName: '',
+    fullName: '',
+    colors: [
+      { name: 'Red' },
+      { name: 'Blue' },
+      { name: 'Green' }
+    ]
   },
   watch: {
     message: function(newValue, oldValue) {
@@ -34,6 +42,25 @@ const monitor = new Vue ({
       this.km = value / 1000000
       this.m = value / 1000
       this.mm = value
+    },
+    firstName: function(value) {
+      this.fullName = value + '' + this.lastName
+    },
+    lastName: function(value) {
+      this.fullName = this.firstName + '' + value
+    },
+    colors: {
+      handler: function(newValue, oldValue) {
+        console.log('Update!')
+      },
+      deep: true,
+      immediate: true
+    }
+  },
+  computed: {
+    FullName: function() {
+      return this.firstName + "" + this.lastName
     }
   }
+
 })
