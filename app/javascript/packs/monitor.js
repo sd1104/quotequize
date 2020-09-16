a28577c7edc34c4ff620982ffcd64ab7a236fa09
sd1104 <67669319+sd1104@users.noreply.github.com>
2020-09-16 22:02:14 +0900

    basic

--- app/javascript/packs/monitor.js
+++ app/javascript/packs/monitor.js
@@ -6,6 +6,34 @@ Vue.config.productionTip = false
 const monitor = new Vue ({
   el: '#monitor',
   data: {
-    message: 'hello'
+    message: 'hello',
+    km: 0,
+    m: 0,
+    mm: 0
+  },
+  watch: {
+    message: function(newValue, oldValue) {
+      console.log(`newValue=${newValue}`)
+      console.log(`oldValue=${oldValue}`)
+      console.log('new: %s, old: %s', newValue, oldValue)
+    },
+    km: function(value) {
+      // console.log(value)
+      this.km = value
+      this.m = value * 1000
+      this.mm = value * 1000000
+    },
+    m: function(value) {
+      // console.log(value)
+      this.km = value / 1000
+      this.m = value
+      this.mm = value * 1000
+    },
+    mm: function(value) {
+      // console.log(value)
+      this.km = value / 1000000
+      this.m = value / 1000
+      this.mm = value
+    }
   }
 })
