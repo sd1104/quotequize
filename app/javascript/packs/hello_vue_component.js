a94948df09383e2f6ae8a4bca12247cb5f9e0730
sd1104 <67669319+sd1104@users.noreply.github.com>
2020-09-17 16:35:08 +0900

    global registration

--- app/javascript/packs/hello_vue_component.js
+++ app/javascript/packs/hello_vue_component.js
@@ -3,6 +3,10 @@ import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
 Vue.config.productionTip = false
 
+Vue.component('hello-component', {
+  template: '<p>hello</p>'
+})
+
 const component = new Vue ({
   el: "#component",
   data: {
