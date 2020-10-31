7205a9f60043e62864fb250258b4f32b0ea5254c
sd1104 <67669319+sd1104@users.noreply.github.com>
2020-10-31 18:57:55 +0900

    small debug

--- app/javascript/packs/hello_vue_bitcoin.js
+++ app/javascript/packs/hello_vue_bitcoin.js
@@ -10,15 +10,16 @@ var bit = new Vue ({
   },
   mounted: function() {
     Axios.get('https://api.coindesk.com/v1/bpi/currentprice.json')
+
     .then(function(response){
-      // console.log(response.data.bpi)
-      // console.log(response.data.bpi.USD.rate_float)
       this.bpi = response.data.bpi
     }.bind(this))
+
     .catch(function(error){
       console.log(error)
       this.haserror = true
     }.bind(this))
+
     .finally(function(){
       this.loading = false
     }.bind(this))
